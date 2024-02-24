082ec98cebd623babd6e74aa40223172146a02e6
Viktoriia <v9bondarenko@gmail.com>
2024-02-24 20:31:19 +0200

    add app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+a = float(input('First number:  '))
+b = float(input('Second number: '))
+operation = input('Operation: ')
+if operation == '+':
+    print(f'{a} + {b} = {a+b}')
+if operation == '-':
+    print(f'{a} - {b} = {a-b}')

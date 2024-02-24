7e25cf7bd85b95969d388008a4b92ca01053c6d4
Viktoriia <v9bondarenko@gmail.com>
2024-02-24 20:50:42 +0200

    add right version of app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-a = float(input('First number:  '))
-b = float(input('Second number: '))
-operation = input('Operation: ')
-if operation == '+':
-    print(f'{a} + {b} = {a+b}')
-if operation == '-':
-    print(f'{a} - {b} = {a-b}')
+def calculate():
+    try:
+        a = float(input('First number:  '))
+        b = float(input('Second number: '))
+        operation = input('Operation: ')
+        match operation:
+            case '+':
+                print(f'{a} + {b} = {a+b}')
+            case '-':
+                print(f'{a} - {b} = {a - b}')
+            case _:
+                print(f"I don`t know this operation yet")
+    except ValueError:
+        print('This is nor a valid number. Do not ruin my program')
+
+
+calculate()
